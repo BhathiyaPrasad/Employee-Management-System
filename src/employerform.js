52c1aec085c67891671e1c10899616be781ac1f7
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-04-22 17:21:17 +0530

    implementing ui with nmaterial ui component Grid

--- src/employerform.js
+++ src/employerform.js
@@ -1,5 +1,47 @@
+import { Grid, Input, Typography } from "@mui/material";
+
 const employerform = props => {
-  
+  return (
+    <Grid
+    container
+    spacing={2}
+    sx={{
+        backgroundColor:"white",
+        marginBottom: "30px",
+        display: "block",
+    }}
+
+    >
+   <Grid item xs={12}>
+    <Typography component={'h1'} sx={{color:"black"}}>Employer's Form</Typography>
+   </Grid>
+   <Grid item xs={12} s={6} sx={{display:"flex"}}>
+    <Typography 
+    component={"label"} 
+    htmlFor="id"
+    sx={{
+        color:"black",
+        marginRight:"20px",
+        fontSize:"16px",
+        width:"100px",
+        display: "block",
+    }}
+    >
+        ID
+    </Typography>
+    <Input 
+    type="number"
+    id="id"
+    name="id"
+    sx={{width:"400px"}}
+    value={""}
+    onChange={ e => {}}
+    
+    />
+   </Grid>
+
+    </Grid>
+  );
 }
 
 export default employerform
