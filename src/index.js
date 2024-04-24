a590677be88db3fe7215fe26846e094dc6486deb
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-04-24 13:31:21 +0530

    test 2.0

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import employers from './employers';
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <BrowserRouter>
-<Route path='/' element={<App />} />
+<Routes>
+<Route path = '/' element={<App />} />
+<Route path = '/employers' element={<employers />} />
+</Routes>
 </BrowserRouter>
 );
 
