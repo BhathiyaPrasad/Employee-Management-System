8ba7d18d09be4060760d7420d8b3c918efa4cea2
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-04-24 17:30:08 +0530

    pass the user typed id to value via React useState hook

--- src/UsersTable.js
+++ src/UsersTable.js
@@ -1,19 +1,46 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 
-const UsersTable = props => {
-   <TableContainer component={Paper}>
-       <Table>
-        <TableHead>
-            <TableRow>
-                <TableCell>ID</TableCell>
-                <TableCell>NAME</TableCell>
-                <TableCell>POSITION</TableCell>
-            </TableRow>
-        </TableHead>
-        <TableBody>
-            
-        </TableBody>
-       </Table>
-   </TableContainer>
+const UsersTable = ({ rows }) => {
+    return(
+    <TableContainer component={Paper}>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell>NAME</TableCell>
+                    <TableCell>POSITION</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {
+                 rows.length > 0 ?   rows.map(row => (
+                        <TableRow key={row.id} sx={{ '&:last-child td,&:last-child th': { border: 0 } }}>
+                            <TableCell component={"th"} scope="row">{row.id}</TableCell>
+                            <TableCell component={"th"} scope="row">{row.name}</TableCell>
+                            <TableCell>
+                                <Button
+                                    sx={{ margin: '0px 10px' }}
+                                    onclick={() => { }}
+                                >
+                                    UPDATE
+                                </Button>
+                                <Button
+                                    sx={{ margin: '0px 10px' }}
+                                    onclick={() => { }}
+                                >
+                                    DELETE
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))
+                   :  <TableRow sx={{ '&:last-child td,&:last-child th': { border: 0 } }}>
+                   <TableCell component={"th"} >No Data</TableCell>
+                   </TableRow>
+                }
+            </TableBody>
+        </Table>
+    </TableContainer>
+    );
 }
+
 export default UsersTable;
