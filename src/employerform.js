a709e9c29afd0a8aad84a436d45dfa0d9ca4dda8
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-04-25 13:56:06 +0530

    update

--- src/employerform.js
+++ src/employerform.js
@@ -1,86 +1,92 @@
-import { Button,Grid, Input, Typography } from "@mui/material";
+import { useState } from "react";
+import { Button, Grid, Input, Typography } from "@mui/material";
+
+
+
 
 const EmployerForm = props => {
-  return (
-    <Grid
-    container
-    spacing={2}
-    sx={{
-        backgroundColor:"white",
-        marginBottom: "30px",
-        display: "block",
-    }}
+    const [id, setId] = useState(0);
+    const [name, setName] = useState('');
+    return (
+        <Grid
+            container
+            spacing={2}
+            sx={{
+                backgroundColor: "white",
+                marginBottom: "30px",
+                display: "block",
+            }}
 
-    >
-   <Grid item xs={12}>
-    <Typography component={'h1'} sx={{color:"black"}}>Employer's Form</Typography>
-   </Grid>
-   <Grid item xs={12} s={6} sx={{display:"flex"}}>
-    <Typography 
-    component={"label"} 
-    htmlFor="id"
-    sx={{
-        color:"black",
-        marginRight:"20px",
-        fontSize:"16px",
-        width:"100px",
-        display: "block",
-    }}
-    >
-        ID
-    </Typography>
-    <Input 
-    type="number"
-    id="id"
-    name="id"
-    sx={{width:"400px"}}
-    value={""}
-    onChange={ e => {}}
-    
-    />
-   </Grid>
-   <Grid item xs={12} s={6} sx={{display:"flex"}}>
-    <Typography 
-    component={"label"} 
-    htmlFor="id"
-    sx={{
-        color:"black",
-        marginRight:"20px",
-        fontSize:"16px",
-        width:"100px",
-        display: "block",
-    }}
-    >
-        NAME
-    </Typography>
-    <Input 
-    type="text"
-    id="name" 
-    name="name"
-    sx={{width:"400px"}}
-    value={""}
-    onChange={ e => {}}
-    
-    />
-   </Grid>
-    <Button
-    sx={{
-        margin:"auto",
-        marginBottom:"20px",
-        backgroundColor:"#00c6e6",
-        color:"black",
-        marginLeft:"20px",
-        marginRight:"20px",
-        "&:hover":{
-            opacity:"0.7",
-            backgroundColor:"#00c6e6",
-        }
-    }}
-    
-    >ADD</Button>
-    
-    </Grid>
-  );
+        >
+            <Grid item xs={12}>
+                <Typography component={'h1'} sx={{ color: "black" }}>Employer's Form</Typography>
+            </Grid>
+            <Grid item xs={12} s={6} sx={{ display: "flex" }}>
+                <Typography
+                    component={"label"}
+                    htmlFor="id"
+                    sx={{
+                        color: "black",
+                        marginRight: "20px",
+                        fontSize: "16px",
+                        width: "100px",
+                        display: "block",
+                    }}
+                >
+                    ID
+                </Typography>
+                <Input
+                    type="number"
+                    id="id"
+                    name="id"
+                    sx={{ width: "400px" }}
+                    value={id}
+                    onChange={e => setId(e.target.value)}
+
+                />
+            </Grid>
+            <Grid item xs={12} s={6} sx={{ display: "flex" }}>
+                <Typography
+                    component={"label"}
+                    htmlFor="id"
+                    sx={{
+                        color: "black",
+                        marginRight: "20px",
+                        fontSize: "16px",
+                        width: "100px",
+                        display: "block",
+                    }}
+                >
+                    NAME
+                </Typography>
+                <Input
+                    type="text"
+                    id="name"
+                    name="name"
+                    sx={{ width: "400px" }}
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+
+                />
+            </Grid>
+            <Button
+                sx={{
+                    margin: "auto",
+                    marginBottom: "20px",
+                    backgroundColor: "#00c6e6",
+                    color: "black",
+                    marginLeft: "20px",
+                    marginRight: "20px",
+                    "&:hover": {
+                        opacity: "0.7",
+                        backgroundColor: "#00c6e6",
+                    }
+                }}
+
+            >ADD</Button>
+
+        </Grid>
+    );
 }
 
 export default EmployerForm;
