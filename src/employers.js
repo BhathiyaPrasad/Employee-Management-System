b0b5d7f5ef6788d3c827578fa8e07e734ed65b2e
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-05-14 11:47:14 +0530

    test

--- src/employers.js
+++ src/employers.js
@@ -1,31 +1,40 @@
 import { Box } from '@mui/material';
-import EmployerForm from './Employerform';
+import EmployerForm from './EmployerForm';
 import UsersTable from './UsersTable';
-
-
-const users = [
-   {
-    id: 1,
-    name: 'Bhathiya',
-   },
-   {
-    id: 2,
-    name: 'Prasad',
-   }
-];
-
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 const Employers = () => {
+  const [Employers, setEmployers] = useState([]);
+ 
+  useEffect(() => {
+   getEmployers();
+  }, []);
+
+
+
+  const getEmployers = () => {
+   axios.get('http://localhost:3001/api/employer')
+   .then(response => {
+      console.log(response);
+   })
+  }
+}
+
+
+
+const Employersdata = () => {
 
  return (
 <Box>
 <EmployerForm />
-<UsersTable rows={users} />
+<UsersTable rows={Employers} />
 </Box>
 
  );
 
     
 }
-export default Employers;
+export default Employersdata;
