07e11e6adf900e22b8f1104e4471d8ceb6285b4f
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-06-02 22:08:14 +0530

    update

--- src/employers.js
+++ src/employers.js
@@ -18,7 +18,7 @@ const Employers = () => {
 
 
   const getEmployers = () => {
-    Axios.get('https://e489-2402-4000-21c0-3a61-525-5c11-819-ec13.ngrok-free.app/api/employer')
+    Axios.get('http://localhost:3001/api/employer')
       .then(response => {
         console.log(response.data?.response || "No Data");
         setEmployers(response.data?.response || "No Data");
@@ -35,7 +35,7 @@ const Employers = () => {
       id: data.id,
       name: data.name,
     }
-    Axios.post('https://e489-2402-4000-21c0-3a61-525-5c11-819-ec13.ngrok-free.app/api/createemployer', payload)
+    Axios.post('http://localhost:3001/api/createemployer', payload)
       .then(() => {
 
         getEmployers()
@@ -55,7 +55,7 @@ const Employers = () => {
       id: data.id,
       name: data.name,
     }
-    Axios.put('https://e489-2402-4000-21c0-3a61-525-5c11-819-ec13.ngrok-free.app/api/updateemployer', payload)
+    Axios.put('http://localhost:3001/api/updateemployer', payload)
       .then(() => {
 
         getEmployers();
@@ -68,7 +68,7 @@ const Employers = () => {
 
   }
   const deleteEmployer = (data) => {
-    Axios.delete('https://e489-2402-4000-21c0-3a61-525-5c11-819-ec13.ngrok-free.app/api/deleteemployer', {data: data})
+    Axios.delete('http://localhost:3001/api/deleteemployer', {data: data})
       .then(() => {
       getEmployers();
       })
